29e6747d717829882c2058b602152daf4a916690
Hubert Denkmair <hubert@denkmair.de>
2017-11-03 18:29:41 +0100

    remove unused code, fix bugs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from machine import Pin
-from display import Display
+from dseg7display import Dseg7Display
 import uhttpd
 import http_file_handler
 import http_api_handler
@@ -8,27 +8,30 @@ import http_api_handler
 class DigitHandler:
     def __init__(self):
         self.number = 0
-        self.color = (10, 10, 10)
-        self.display = Display(Pin(2))
-        self.display.set(self.number, self.color)
+        self.r = 10
+        self.g = 10
+        self.b = 10
+        self.pin = Pin(2)
+        self.pin.init(self.pin.OUT)
+        self.display = Dseg7Display(self.pin)
+        self.display.set_number(self.number, self.r, self.g, self.b)
+        self.display.write()
 
     def get(self, req):
-        r, g, b = self.color
-        return {'num': self.number, 'r': r, 'g': g, 'b': b}
+        return {'num': self.number, 'r': self.r, 'g': self.g, 'b': self.b}
 
     def post(self, req):
         params = req['body']
-        r, g, b = self.color
         if 'num' in params:
             self.number = int(params['num'])
         if 'r' in params:
-            r = max(min(int(params['r']), 255), 0)
+            self.r = max(min(int(params['r']), 255), 0)
         if 'g' in params:
-            g = max(min(int(params['g']), 255), 0)
+            self.g = max(min(int(params['g']), 255), 0)
         if 'b' in params:
-            b = max(min(int(params['b']), 255), 0)
-        self.color = (r, g, b)
-        self.display.set(self.number, self.color)
+            self.b = max(min(int(params['b']), 255), 0)
+        self.display.set_number(self.number, self.r, self.g, self.b)
+        self.display.write()
         return self.get(req)
 
 
